3220eefcf7b20a06e2d5245c4d3a5b51510f443e
Master <dkp131292@gmail.com>
2026-01-11 15:30:14 +0530

    final

--- users.js
+++ users.js
@@ -1,4 +1,3 @@
-const users = [
-  { username: "deepak", password: "12345", name: "Deepak Pandey" },
-  { username: "admin", password: "admin", name: "Administrator" }
+let users = JSON.parse(localStorage.getItem("users")) || [
+  { username: "deepak", password: "12345", name: "Deepak Pandey", photo: "https://cdn-icons-png.flaticon.com/512/3135/3135715.png" }
 ];
